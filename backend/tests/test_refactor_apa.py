d368f7b83aa3c07ace79fb4a3c07379bb9130c30
y00tss <shepel9119@gmail.com>
2024-11-15 13:33:47 +0300

    flake8 refactor for tests

--- backend/tests/test_refactor_apa.py
+++ backend/tests/test_refactor_apa.py
@@ -2,7 +2,6 @@ import pytest
 from unittest.mock import AsyncMock
 from articles.article_service.mapper_type import DocumentWorkFlowFactory
 
-
 TEMP_DIR = "articles/documents/test_user"
 
 
@@ -15,17 +14,17 @@ async def test_document_process_success():
     # mock_doc = MagicMock()
     # mock_doc.start_flow = AsyncMock()
     # mock_doc.create_report = AsyncMock(return_value="No issues found.")
-    # mock_doc.get_updated_document = AsyncMock(return_value=f"{TEMP_DIR}/document.docx")
+    # mock_doc.get_updated_document = AsyncMock(return_value=f"{TEMP_DIR}/document.docx") # noqa
 
     # Mocking the factory method to return our mocked document object
     # DocumentWorkFlowFactory.create_workflow = AsyncMock(return_value=mock_doc)
     pass
 
 
-
 @pytest.mark.asyncio
 async def test_document_process_failure():
     # Mocking the DocumentWorkFlowFactory to raise an exception
-    DocumentWorkFlowFactory.create_workflow = AsyncMock(side_effect=Exception("Document processing failed"))
+    DocumentWorkFlowFactory.create_workflow = AsyncMock(
+        side_effect=Exception("Document processing failed")
+    )
     pass
-
